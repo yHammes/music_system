bf13d9d2860cd112be71bb9288f3ce14ee89788e
yHammes <hammes342@gmail.com>
2024-05-01 13:00:39 -0300

    fixed debug log text

--- server/util.lua
+++ server/util.lua
@@ -1,7 +1,7 @@
 function log(msg)
     local msg = msg:gsub("#%x%x%x%x%x%x", "");
     if (Config.logs.debug) then
-        outputDebugString(msg, 4, 255, 255, 255)
+        outputDebugString('[MusicPanel]' .. msg, 4, 255, 255, 255)
     end
     if (Config.logs.discord) then
         fetchRemote(Config.logs.discordWebhook, {
